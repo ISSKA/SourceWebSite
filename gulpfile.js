f9552536075a71f9d5805700168714ca6461a87e
Sébastien <popod@me.com>
2019-11-13 17:25:19 +0100

    Add gulp script to automatically deploy the dist folder in production

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const ftp = require('vinyl-ftp')
+
+require('dotenv').config()
+
+/** Configuration **/
+const user = process.env.FTP_USER
+const password = process.env.FTP_PASSWORD
+const host = 'ops.ftp.infomaniak.com'
+const port = 21
+const localFilesGlob = ['./dist/**/*']
+const remoteFolder = '/'
+
+// helper function to build an FTP connection based on our configuration
+function getFtpConnection() {
+  return ftp.create({
+    host,
+    port,
+    user,
+    password,
+    parallel: 5,
+    log: gutil.log
+  })
+}
+
+/**
+ * Deploy task.
+ * Copies the new files to the server
+ */
+gulp.task('deploy', function() {
+  const conn = getFtpConnection()
+
+  return gulp
+    .src(localFilesGlob, { base: './dist', buffer: false })
+    .pipe(conn.newer(remoteFolder)) // only upload newer files
+    .pipe(conn.dest(remoteFolder))
+})
