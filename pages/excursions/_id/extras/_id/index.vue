beab710aedca4b92f644acdfb95af49accfb1a54
Sébastien <popod@me.com>
2019-11-18 18:05:18 +0100

    Add excursion default extra page

---
+++ pages/excursions/_id/extras/_id/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Excursion 1 - Extra x
+      </h1>
+      <div>
+        ...
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
