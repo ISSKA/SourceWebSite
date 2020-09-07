682405817b78512503c1b4a47cee2e4e42a99eee
PelletierColin <69189297+PelletierColin@users.noreply.github.com>
2020-09-07 16:10:48 +0200

    Update excursion-2.js

--- locales/fr/excursion-2.js
+++ locales/fr/excursion-2.js
@@ -3,7 +3,7 @@ export default {
   subtitle: 'Sefinental, BE',
   summary: {
     difficulty: 'xxx',
-    duration: 'xxx heures',
+    duration: '5h30',
     download_file: 'xxx'
   },
   source: {
@@ -19,20 +19,25 @@ export default {
   description: [
     {
       label: 'xxx',
-      details: 'xxx'
+      details: 'Blottie au fond d’une vallée sauvage, la source de Chilchbalm est très impressionnante. Elle jaillit en pleine falaise à environ 30 mètres du sol. A ses pieds, un petit glacier occupe le fond de la vallée de Sefinen et, au-dessus d’elle, le Gspaltenhorn la domine du haut de ses 3436 m d’altitude. Les orifices d’où jaillissent l’eau ne peuvent s’atteindre qu’au prix d’une escalade engagée. Il y a peu de temps encore, cette source était dissimulée par la masse du glacier. C’est grâce à la fonte de celui-ci qu’on peut aujourd’hui la voir et l’admirer de loin. A ce jour, on sait peu de choses sur cette source, si ce n’est qu’elle est très belle et mérite le détour ! L’origine précise de ses eaux (bassin d’alimentation) n’est pas (encore) connue des hydrogéologues et les spéléologues n’ont pas (encore) pu explorer cette source – pour autant que cela soit techniquement possible...'
     }
   ],
   route: {
     details: {
-      start: 'xxx',
-      end: 'xxx',
-      distance: 'xxx km',
-      delta_height: '+ xxx m / – xxx m',
-      duration: 'xxx heures',
-      restauration: 'xxx',
-      type: 'xxx'
+      start: 'Car postal de Lauterbrunnen à Rütti/Stechelberg',
+      end: 'Stechelberg',
+      distance: '13 km',
+      delta_height: '+ 800 m / – 800 m',
+      duration: '5h30 (sans les pauses)',
+      restauration: 'Restaurant à Rütti/Stechelberg',
+      type: 'Randonnée en moyenne montagne, sans difficultés particulières'
     },
-    description: ['xxx']
+    description: [
+      'Franchir le pont qui enjambe la Weisse Lütschine et prendre le sentier en lacets qui monte dans la forêt à proximité des Sefinenfälle.'
+      'Parvenu au lieu-dit Wasserbrigg, continuer en rive gauche de la rivière Sefinen Lütschine sur environ 4 kilomètres. Laisser sur la droite le sentier qui monte à la Rotstockhütte et continuer à remonter le cours d’eau.'
+      'A l’approche de la moraine, le sentier devient moins bien balisé mais la vue, au loin, des belles cascades jaillissant de la falaise permet de se diriger dans la bonne direction.'
+      'Selon les conditions climatiques, il est préférable de ne pas trop s’approcher de la source en raison du risque de chutes de pierre provenant de la paroi nord du Gspaltenhorn.'
+      'Le retour se fait par le même chemin, à moins que l’on ne souhaite prolonger la randonnée par un passage à la Rotstockhütte.']
   },
   more: [
     {
@@ -49,9 +54,12 @@ export default {
   },
   extras: [
     {
-      title: 'xxx',
+      title: 'La source de Chilchbalm',
       sumary: '',
-      description: ['xxx'],
+      description: [
+        'Haut dans la vallée de Lauterbrunnen, Gimmelwald est la porte d’une vallée latérale, le Sefinental. Dominée au nord par le Schilthorn et au sud par l’Ellstabhorn, cette vallée est parcourue par la Sefinen Lütschine, un torrent montagnard comme bien d’autres. Prenez le temps de remonter son cours en direction du Gspaltenhorn, qui ferme la vallée à l’ouest avec ses 3436 m. C’est tout au fond du cirque rocheux que vous découvrirez après une longue marche loin du bruit et du monde un lieu merveilleux et mystérieux. Chilchbalm est une source spectaculaire d’où coule une eau cristalline et glacée. On n’en sait pas plus à son sujet, mais a-t-on vraiment besoin de tout expliquer ? Ici, c’est le coup d’oeil et l’ambiance qui priment : allez-y et faites-y le plein de beauté et d’émotion. Garanti sans regrets...'
+        'Un lieu, un prétexte, une occasion de se laisser aller à un peu de philosophie. Qu’est-ce qu’une source ? D’où vient-elle ? Que se cache-t-il derrière ? Est-ce une porte de l’enfer souterrain ? Ou plutôt l’émergence de toute vie ? Quels génies bienfaisants y logent-ils ? A moins que ce soient de vilains démons ?'
+        'L’homme invente ses propres légendes à sa mesure ; ce n’est pas un hasard si les sources sont, plus souvent que d’autres lieux, l’endroit où elles s’expriment...'],
       images: [
         {
           legend: 'xxx',
