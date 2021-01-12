513e1677b170b20a12bb0c938eb6e88d4a190b88
PelletierColin <69189297+PelletierColin@users.noreply.github.com>
2021-01-12 12:12:42 +0100

    Add Bois du Jorat

--- locales/de/excursion-19.js
+++ locales/de/excursion-19.js
@@ -1,42 +1,39 @@
 export default {
-  title: 'Die Quellen von Combioula',
-  subtitle: 'St-Martin, VS',
+  title: 'Bois du Jorat',
+  subtitle: 'Lausanne, VD',
   summary: {
     difficulty: 'xxx',
     duration: 'xxx heures',
-    download_file: 'combioula_de.pdf'
+    download_file: 'bois_du_jorat_de.pdf'
   },
   link_swissmobile: 'https://map.schweizmobil.ch/?lang=fr&land=wanderland&route=all&photos=no&bgLayer=pk&logo=no&season=summer&resolution=3.68&E=2599148&N=1113709&layers=Wanderland&trackId=7058691',
   source: {
     coordinates: [2598200, 1115000],
     position: {
-      x: 1207,
-      y: 1931,
+      x: 671,
+      y: 1522,
       r: 55
     },
-    name: 'Sources chaudes de Combioule',
-    canton: 'VS'
+    name: 'Chemin des fontaines des Bois du Jorat',
+    canton: 'VD'
   },
   description: [
-    '<Das Val d‘Hérens ist bekannt für seine hohen Gipfel(Dent Blanche, Grand Cornier, Dent d’Hérens ...) und für seine kuriose Geomorphologie, die von den Pyramiden von Euseigne geprägt ist.',
-    'Doch weiter unten im Tal versteckt sich entlang der Borgne ein weiteres Highlight der Natur: die warmen Quellen von Combioula, in denen man ungestört baden kann...zumindest bislang! Ein Tourismusprojekt könnte die Situation in den kommenden Jahren verändern.',
-    'Bei der hier vorgestellten, angenehmen Wanderung können Sie nicht nur diese Quellen, sondern auch weitere Pyramiden, die nicht auf allen Postkarten abgebildet sind, erkunden. Unterwegs überqueren wir eine eindrückliche Brücke, welche die beiden Seiten einer tiefen Schlucht verbindet, und wir besichtigen einen verlassenen Weiler, der vor Kurzem zu einem Agrotourismus-Ort umgewandelt wurde.'
-  ],
+    'Auf dem anlässlich der Weltwald- und Wassertage am 21. und 22. März 2003 geschaffenen Brunnenpfad kann man den Wald und die Quellen des Bois du Jorat auf einem markierten Rundweg mit anschaulichen Erläuterungstafeln erkunden.',
+'Der Wanderweg wurde vom Wasserwerk der Stadt Lausanne entwickelt und gestaltet.Er bietet die Möglichkeit, mehrere Quellen, die aus Molassegestein stammen, zu entdecken.'],
   route: [{
     details: {
-      start: 'Euseigne',
-      end: 'Euseigne',
-      distance: '11 km',
-      delta_height: '+ 649 m / – 649 m',
-      duration: '4 Stunden',
-      restauration: 'Weiler von Ossone, Euseigne',
-      type: 'Wanderung ohne besondere Schwierigkeiten, aber einige Passagen in recht steilem Gelände(+ sicherer, aber luftiger Steg!)'
+      start: 'Chalet-à-Gobet',
+      end: 'Chalet-à-Gobet',
+      distance: '12 km',
+      delta_height: '+ 236 m / – 236 m',
+      duration: '3h30',
+      restauration: 'Chalet des Enfants',
+      type: 'Angenehme Wanderung ohne besondere Schwierigkeiten über von Waldweg, realisierbar zu Fuss oder mit dem Velo'
     },
     description: [
-      'Unsere Rundwanderung beginnt im Dorfzentrum von Euseigne.Wir gehen Richtung La Luette, vorzugsweise nicht auf der Strasse, sondern auf dem Fusspfad, der durch den Weiler La Crêta führt.',
-      'In La Luette steigen wir bis zum Fluss ab und begeben uns auf die kleine Strasse, die nach links abzweigt.Etwas weiter wird aus der Strasse ein Fussweg, der sich am Berghang bis nach Ossone entlangschlängelt.Nachdem wir diesen Weiler passiert haben, gehen wir weiter in derselben Richtung bis zur Manna-Schlucht. Dort steigen wir zum Fluss hinab.Um zu den Quellen zu gelangen, muss man den Fluss überqueren, nach rechts abbiegen, über die nächsten Brücke zurück auf die rechte Uferseite und noch etwa weitere hundert Meter flussabwärts laufen.',
-      'Der Rückweg nach Euseigne führt durch das Tal, in dem die Dixence fliesst.Vergessen Sie nicht, auf dem Weg öfters in die Höhe zu blicken und hie und da die pyramidenförmigen Felsen zu bewundern.'
-    ]
+      'Die Brunnenwanderung beginnt in Chalet-à-Gobet oberhalb von Lausanne.Der markierte Wanderweg bietet eine Rundwanderung im Herzen des Bois du Jorat.',
+'Der Pfad führt den Wanderer an einem Dutzend Brunnen vorbei, die von nahe gelegenen Quellen gespeist werden.',
+'An jedem Brunnen findet man eine Informationstafel mit der Beschreibung der entsprechenden Quelle und Auskünften über die Wasserqualität und die Geschichte der Brunnen.']
   },
     ],
 
@@ -54,58 +51,6 @@ export default {
     link_end: 'https://www.sbb.ch/fr/acheter/pages/fahrplan/fahrplan.xhtml?von=Euseigne,village'
   },
   extras: [
-    {
-      title: 'Diskrete warme Quellen',
-      cover: 'img-2.jpg',
-      sumary: '',
-      description: [
-        'Das Val d‘Hérens wird oft auch Teufelstal genannt. Unsere Vorfahren hielten die warmen Quellen, die in Combioula sprudeln, wohl für Teufelswerk. Zahlreiche warme Mineralquellen entspringen am Rand des Flussbetts der Borgne. Diese Quellen (es sind insgesamt 81!) sind diskret im Tal verborgen und doch schon seit langem bekannt.',
-        'Das aus geologischen Tiefenschichten stammende Wasser hat eine Temperatur von bis zu 29 °C.Der hohe Schwefelgehalt verleiht ihm einen starken Geruch nach faulen Eiern, und die Eisenoxide färben es rot.',
-        'In einem Manuskript aus dem 16. Jahrhundert wurden diese mineralstoffhaltigen Quellen zum ersten Mal genannt.Anfang des 18. Jahrhunderts versuchte man, die Mineralsalze dieser Wässer durch Extraktion zu nutzen.Es wurden verschiedene Stollen gegraben, doch diese Versuche mussten mangels finanzieller Mittel nach und nach wieder eingestellt werden. 1818 stellte der Staat Wallis dieses Unterfangen endgültig ein. 1963 wurde der Zugang zu den Stollen nach einem Deichbruch mit Geschiebe zugeschüttet.',
-        '1986 und 2016 wurden Erkundungsbohrungen durchgeführt mit dem Ziel, die Quellen von Combioula für ein Thermalbad zu nutzen.Die letzte Bohrung in eine Tiefe von fast 1000 Metern bestätigte schliesslich die Hypothese der Hydrogeologen über die Existenz eines wertvollen Grundwasserleiters in 390 bis 550 Metern Tiefe, der warmes sulfat -, chlorid -, magnesium - und natriumreiches Wasser enthält. 2019 wurde der Konzessionsvertrag an die Projetkträger der Therme der Dixence von den Gemeinden Hérémence und Saint - Martin bewilligt.'
-      ],
-      images: [
-        {
-          legend: '',
-          name: 'img-1.jpg'
-        },
-        {
-          legend: 'Eine der Quellen der Combioula im Bett der Borgne.',
-          name: 'img-2.jpg'
-        },
-
-      ],
-      onTheRoad: true,
-      position: {
-      x: 627,
-      y: 77,
-      },
-    },
-    {
-      title: 'Die Pyramiden von Euseigne',
-	  cover: 'img-1.jpg',
-      sumary: '',
-      description: [
-        'Die Pyramiden von Euseigne sind zwar berühmt, aber sie sind nur ein teilweise zementiertes Überbleibsel dieser Art natürlicher Kuriositäten. Man nennt sie auch „cheminées de fées“ (Feenschornsteine) oder „demoiselles coiffées“ (Jungfrauen mit Kopfhaube).',
-        'Wenn ein Gletscher einen Geröllhaufen, vor und mit sich her schiebt, bleibt dieser als Moräne liegen, wenn der Gletscher infolge einer meist natürlichen Klimaerwärmung zu schmelzen beginnt.Diese verschiedenartigen mächtigen Ablagerungen, die feine oder kiesige Sedimente sowie mehr oder weniger grobes Gestein enthalten, sind der Witterung ausgesetzt und werden vom Regenwasser angegriffen, weil sie recht locker liegen.Der Regen wäscht das Gestein aus, und wenn ein grosser Gesteinsblock freigelegt wird, dient er als Beschützer der darunterliegenden Sedimentsäule.Er agiert wie eine Schutzhaube und verhindert bzw.verlangsamt den Untergang des verbleibenden Reliefs.Manchmal ist der Schutzblock noch sichtbar, manchmal rutscht er herab und verliert seine schützende Eigenschaft.Diese geomorphologischen Besonderheiten sind also vergängliche Formen mit einer Lebensdauer von Jahrhunderten bis Jahrtausenden.',
-        'Bei dieser Wanderung bekommt man einige weitere „Jungfrauen mit Haube“ zu sehen, die jünger und natürlicher aussehen als jene auf den Postkarten.Sie müssen nur gegen Südwesten hinaufblicken oder auf die andere Seite des Borgne - Tals in Richtung Saint - Martin(Suen) schauen.Es ist auch vorhersehbar, dass einige der Alphütten, die auf dem Bergkamm nahe des Abgrundes stehen, keine allzu lange Lebenserwartung mehr haben...'
-      ],
-      images: [
-        {
-          legend: 'Pyramiden unterhalb von Euseigne...',
-          name: 'img-1.jpg'
-        },
-        {
-          legend: '... und unterhalb von Hérémence.',
-          name: 'img-2.jpg'
-        },
-       
-      ],
-      onTheRoad: true,
-      position: {
-        x: 667,
-        y: 379,
-      },
-    },
+    
   ]
 }
--- locales/fr/excursion-19.js
+++ locales/fr/excursion-19.js
@@ -1,41 +1,40 @@
 export default {
-  title: 'Sources de Combioula',
-  subtitle: 'St-Martin, VS',
+  title: 'Bois du Jorat',
+  subtitle: 'Lausanne, VD',
   summary: {
     difficulty: 'xxx',
     duration: 'xxx heures',
-    download_file: 'combioula_fr.pdf'
+    download_file: 'bois_du_jorat_fr.pdf'
   },
   link_swissmobile: 'https://map.schweizmobil.ch/?lang=fr&land=wanderland&route=all&photos=no&bgLayer=pk&logo=no&season=summer&resolution=3.68&E=2599148&N=1113709&layers=Wanderland&trackId=7058691',
   source: {
     coordinates: [2598200, 1115000],
     position: {
-      x: 1207,
-      y: 1931,
+      x: 671,
+      y: 1522,
       r: 55
     },
-    name: 'Sources chaudes de Combioule',
-    canton: 'VS'
+    name: 'Chemin des fontaines des Bois du Jorat',
+    canton: 'VD'
   },
   description: [
-    '<b>Le val d’Hérens</b> est célèbre pour ses hauts sommets(Dent Blanche, Grand Cornier, Dent d’Hérens...) et pour la curiosité géomorphologique que constituent les pyramides d’Euseigne.',
-    'Mais, plus bas, le long de la Borgne, se cache un autre point d’intérêt naturel : les sources chaudes de Combioula dans lesquelles on peut se baigner librement...du moins pour l’instant! En effet, un projet d’exploitation touristique pourrait changer la donne dans les prochaines années.',
-    'L’excursion proposée ici est agréable et permet de découvrir non seulement ces sources mais également d’autres pyramides que celles qui figurent sur toutes les cartes postales.Au passage, on franchit une passerelle impressionnante qui lie les deux rives d’un profond ravin et on visite un hameau abandonné qui a fait récemment l’objet d’une réhabilitation agro - touristique.'
+    'Le chemin des fontaines, créé à l’occasion des journées mondiales de la forêt et de l’eau les 21 et 22 mars 2003, va à la découverte de la forêt, des sources des <b>Bois du Jorat</b> à l’aide d’un parcours fléché et de panneaux explicatifs et néanmoins sympathiques.',
+    'Cet itinéraire a été conçu et imaginé par le Service des eaux de la ville de Lausanne.Il permet de découvrir plusieurs sources issues de roches molassiques.'
   ],
   route: [{
     details: {
-      start: 'Euseigne',
-      end: 'Euseigne',
-      distance: '11 km',
-      delta_height: '+ 649 m / – 649 m',
-      duration: '4 heures',
-      restauration: 'Hameau de Ossone, Euseigne',
-      type: 'Randonnée sans difficulté particulière, mais quelques passages dans des terrains assez pentus (+ passerelle sûre mais aérienne!)'
+      start: 'Chalet-à-Gobet',
+      end: 'Chalet-à-Gobet',
+      distance: '12 km',
+      delta_height: '+ 236 m / – 236 m',
+      duration: '3h30',
+      restauration: 'Chalet des Enfants',
+      type: 'Randonnée agréable et sans difficultés sparticulières à réaliser à pied ou à vélo, le long de chemins forestiers'
     },
     description: [
-      'L’excursion en boucle proposée ici débute au centre du village d’Euseigne.Prendre la direction de La Luette en évitant de suivre la route au profit du sentier pédestre qui passe par le hameau de La Crêta.',
-      'A La Luette, descendre jusqu’à la rivière et prendre la petite route qui part sur la gauche.Plus loin, celle - ci se transforme en un sentier qui ondule à flanc de coteau jusqu’à Ossone.Après avoir traversé ce hameau, poursuivre dans la même direction jusqu’au ravin de la Manna.De là, descendre jusqu’à la rivière.Pour trouver les sources, il faut traverser la rivière, tourner à droite, revenir sur la rive droite au prochain pont et, continuer encore une centaine de mètres vers l’aval.',
-      'Retour à Euseigne en traversnt le vallon où coule La Dixence.En cours de route, ne pas oublier de lever le nez pour admirer les roches en forme de pyramides qui se trouvent ça et là.'
+      'La randonnée débute au Chalet-à-Gobet, sur les hauteurs de Lausanne.L’itinéraire fléché permet d’effectuer une boucle au coeur des Bois du Jorat.',
+'Le long du parcours, on passe à côté d’une dizaine de fontaines alimentées par des sources proches.',
+'A chaque fontaine, on trouve un panneau d’information qui décrit la source correspondante et donne des renseignements sur la qualité de l’eau et l’histoire des fontaines.'
     ]
   },
     ],
@@ -54,58 +53,6 @@ export default {
     link_end: 'https://www.sbb.ch/fr/acheter/pages/fahrplan/fahrplan.xhtml?von=Euseigne,village'
   },
   extras: [
-    {
-      title: 'Des sources chaudes et bien discrètes',
-      cover: 'img-2.jpg',
-      sumary: '',
-      description: [
-        'Le Val d’Hérens est souvent dénommé Vallée du diable. Nos ancêtres y ont peut-être cru en constatant que des eaux chaudes surgissent à Combioula. De nombreuses sources thermo-minérales émergent en bordure du lit de la Borgne. Situées discrètement au fond de la vallée, ces sources - on en compte pas moins de 81 ! - sont cependant connues depuis fort longtemps.',
-        'A leur sortie, les eaux issues des couches géologiques profondes, affichent une température atteignant 29°C et leur haute teneur en soufre leur confère une forte odeur d’oeuf pourri, alors que les oxydes de fer les teintent en rouge.',
-        'C’est dans un manuscrit du XVIe siècle que sont pour la première fois relatées ses sources riches en sels minéraux.On a bien tenté au début du XVIIIe siècle, de mettre en valeur ces eaux en extrayant les nombreux sels qu’elles contiennent.Diverses galeries ont été creusées, mais faute de moyens financiers, ces tentatives ont dû peu à peu être interrompues.En 1818, l’Etat du Valais cessa définitivement les recherches et l’entrée des galeries fut obstruée en 1963 par les alluvions déposées suite à la rupture d’une digue.',
-        'Des forages de reconnaissance ont été réalisés en 1986, puis de nouveaux en 2016, dans le but d’utiliser les sources de Combioula pour un centre thermal.Confirmant les hypothèses hydrogéologiques existantes, le dernier forage creusé jusqu’à près de 1000 mètres de profondeur révéla la présence d’un précieux aquifère situé entre 390 et 550 mètres de profondeur, contenant de l’eau chaude riche en sulfates, chlorures, magnésium et sodium.En 2019, les communes d’Hérémence et de Saint - Martin ont approuvé l’acte de concession des eaux thermales de Combioula aux porteurs du projet des Thermes de la Dixence.'
-      ],
-      images: [
-        {
-          legend: '',
-          name: 'img-1.jpg'
-        },
-        {
-          legend: 'L’une des sources de Combioula, sommairement aménagée dans le lit de La Borgne.',
-          name: 'img-2.jpg'
-        },
-
-      ],
-      onTheRoad: true,
-      position: {
-      x: 440,
-      y: 37,
-      },
-    },
-    {
-      title: 'Les pyramides d’Euseigne',
-	  cover: 'img-1.jpg',
-      sumary: '',
-      description: [
-        'Les pyramides d’Euseigne sont certes célèbres mais ne sont qu’un vestige en partie cimenté de ce type de curiosités naturelles. On les appelle aussi cheminées de fées ou, ce qui est plus explicite, demoiselles coiffées.',
-        'Lorsque qu’un glacier pousse devant lui et sur ses bords ces tas de débris variés que l’on appelle moraines, il les abandonne sur place s’il vient à fondre à la suite du réchauffement, principalement naturel, du climat.Ces épais dépôts hétérogènes, contenant en vrac des sédiments fins ou graveleux et des blocs plus ou moins gros, sont ensuite attaqués par le ruissellement des eaux météoriques, et ce d’autant plus facilement qu’ils sont souvent assez meubles.La pluie ravine et creuse mais, lorsqu’un bloc important est dégagé, celui devient le protecteur de la colonne de sédiments située en dessous.Il agit comme un chapeau et empêche – ou du moins ralentit – la disparition de ce relief résiduel.Parfois le bloc protecteur est encore visible, parfois il glisse vers le bas et perd son rôle particulier.Ces curiosités géomorphologiques sont donc des formes éphémères à l’échelle des siècles et des millénaires.',
-        'Lors de cette promenade, on peut voir quelques autres demoiselles coiffées, plus fraîches et plus naturelles que celles célébrées par le tourisme de cartes postales, en levant les yeux vers le sud - ouest ou en regardant de l’autre côté de la vallée de la Borgne, en direction du hameau de St - Martin(Suen).On peut d’ailleurs prévoir que certains mazots perchés sur la crête, au bord du précipice, n’ont pas une bien longue espérance de survie...'
-      ],
-      images: [
-        {
-          legend: 'Pyramides au-dessous d’Euseigne...',
-          name: 'img-1.jpg'
-        },
-        {
-          legend: '... et au-dessous d’Hérémence.',
-          name: 'img-2.jpg'
-        },
-       
-      ],
-      onTheRoad: true,
-      position: {
-        x: 465,
-        y: 251,
-      },
-    },
+    
   ]
 }
